7f232f8f943837ec9125987f7db58363f236362a
Joshua Ohm <j.ohm@travispattern.com>
2019-11-19 13:13:56 -0800

    Changes to integrate Passport

--- php/app/User.php
+++ php/app/User.php
@@ -5,8 +5,9 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
 
