fb34ed9280dfebc148863dc4b8d89b8623dc0a36
Joshua Ohm <joshua.ohm@gmail.com>
2019-11-20 17:21:30 +0000

    added ships table migration

---
+++ php/database/migrations/2019_11_20_170434_create_ships_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ships', function (Blueprint $table) {
+            $table->bigIncrements('id');
+	    $table->string('manufacturer', 100);
+	    $table->string('name', 100);
+	    $table->json('crewPositions');
+	    $table->integer('passengerSeats')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ships');
+    }
+}
