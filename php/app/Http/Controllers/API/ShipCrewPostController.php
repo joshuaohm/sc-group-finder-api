fba74cc2a655503905762a746252849d7bf151ac
Joshua Ohm <j.ohm@travispattern.com>
2019-11-20 08:50:41 -0800

    Fixing some errors in ShipCrewPosts

--- php/app/Http/Controllers/API/ShipCrewPostController.php
+++ php/app/Http/Controllers/API/ShipCrewPostController.php
@@ -17,7 +17,7 @@ class ShipCrewPostController extends BaseController
    */
   public function index()
   {
-      $scPosts = ShipCrewPosts::where('is_active', true);
+      $scPosts = ShipCrewPost::where('is_active', true);
   
       return $this->sendResponse(ShipCrewPostResource::collection($scPosts), 'Ship Crew Posts retrieved successfully.');
   }
