292d25ff7b873037eef11f113f8ad2324e133d55
Joshua Ohm <joshua.ohm@gmail.com>
2019-11-19 19:57:03 -0800

    more passport

--- php/routes/api.php
+++ php/routes/api.php
@@ -13,9 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::post('/register', 'API\RegisterController@register');
-Route::post('/login', 'API\RegisterController@login');
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/register', 'API\RegisterController@register');
+    Route::post('/login', 'API\RegisterController@login');  
+});
    
 Route::middleware('auth:api')->group( function () {
     Route::resource('ship_crew_posts', 'API\ShipCrewPostController');
 });
+
