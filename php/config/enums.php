0d370d779b9b055282addec74a026a9e88cddd84
Joshua Ohm <j.ohm@travispattern.com>
2019-12-06 16:27:03 -0800

    fixed error in enums

--- php/config/enums.php
+++ php/config/enums.php
@@ -1,9 +1,11 @@
+<?php
+
 return [
-'location_types' => [
-'UNKNOWN' => 0,
-'SYSTEM' => 1,
-'STAR' => 2,
-'BODY' => 3,
-'LANDINGZONE' => 4,
-]
+  'location_types' => [
+    'UNKNOWN' => 0,
+    'SYSTEM' => 1,
+    'STAR' => 2,
+    'BODY' => 3,
+    'LANDINGZONE' => 4,
+  ]
 ];
