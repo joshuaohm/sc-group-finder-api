7a30a7d835b7d2ab131f3b7114568992a8742ac7
Joshua Ohm <j.ohm@travispattern.com>
2019-12-13 14:23:41 -0800

    this is hell

--- php/database/seeds/ShipsTableSeeder.php
+++ php/database/seeds/ShipsTableSeeder.php
@@ -65,7 +65,7 @@ class ShipsTableSeeder extends Seeder
         for ($i = 0; $i < count($ship); $i++) {
           \App\ShipPosition::insert([
             "ship" => $temp,
-            "position" => Position::where("type", $ship[$i]["type"])->where("location", isset($ship[$i]["location"]) ? $ship[$i]["location"] : null)->first()->id;
+            "position" => Position::where("type", $ship[$i]["type"])->where("location", isset($ship[$i]["location"]) ? $ship[$i]["location"] : null)->first()->id
           ]);
         }
       }
